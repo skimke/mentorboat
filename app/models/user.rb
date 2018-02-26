41512f31e8465c3c5a3b1e702a77648f4df66bf6
Stella Kim <stella.kim@icloud.com>
2018-02-25 21:32:17 -0500

    Add shorter method User#experience to access experience_in_years

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :experience_in_years, presence: true
+
+  def experience
+    experience_in_years
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,4 +5,13 @@ class UserTest < ActiveSupport::TestCase
   should validate_presence_of(:email)
   should validate_uniqueness_of(:email)
   should validate_presence_of(:experience_in_years)
+
+  setup do
+    @user = build_stubbed(:user)
+  end
+
+  test '#experience returns experience_in_years' do
+    expected = @user.experience_in_years
+    assert_equal expected, @user.experience
+  end
 end
