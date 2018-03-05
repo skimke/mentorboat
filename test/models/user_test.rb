7b47a69ec517a06a518ed591fde348175c40a0ed
Stella Kim <stella.kim@icloud.com>
2018-03-04 22:39:23 -0500

    User#full_name method concats first and last name fields

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,12 +1,18 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  should validate_presence_of(:name)
+  should validate_presence_of(:first_name)
+  should validate_presence_of(:last_name)
   should validate_uniqueness_of(:email).allow_blank
   should validate_presence_of(:experience_in_years)
 
   setup do
     @user = create(:user, password: 'supersafepassword123')
+  end
+
+  test '#full_name returns first and last name joined with a space' do
+    expected = 'Hermione Granger'
+    assert_equal expected, @user.full_name
   end
 
   test '#experience returns experience_in_years' do
