56602107e508d570a1a84317bb14dc0a737e43b2
Stella Kim <stella.kim@icloud.com>
2018-03-04 22:44:09 -0500

    Add is_admin column with default false to User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180305032106) do
+ActiveRecord::Schema.define(version: 20180305034300) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20180305032106) do
     t.string "remember_token", limit: 128
     t.string "first_name"
     t.string "last_name"
+    t.boolean "is_admin", default: false
     t.index ["email"], name: "index_users_on_email"
     t.index ["remember_token"], name: "index_users_on_remember_token"
   end
