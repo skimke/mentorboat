2596b6244cd3ac6fd2f9b4b19365d80ac34cc594
Stella Kim <stella.kim@icloud.com>
2018-03-31 17:02:52 -0400

    Add Approval flow for Applications

--- test/controllers/applications_controller_test.rb
+++ test/controllers/applications_controller_test.rb
@@ -60,4 +60,96 @@ class ApplicationsControllerTest < ActionDispatch::IntegrationTest
 
     assert_response :success
   end
+
+  test "#approve is blocked for non-admin users" do
+    user = create(:user)
+
+    post session_url, params: { session: { email: user.email, password: user.password } }
+
+    get approve_application_url(user)
+
+    assert_redirected_to user_url(user)
+  end
+
+  test "#approve is viewable for admin users" do
+    user = create(:user, :admin)
+
+    post session_url, params: { session: { email: user.email, password: user.password } }
+
+    get approve_application_url(user)
+
+    assert_response :success
+  end
+
+  test "#update is blocked for non-admin users" do
+    user = create(:user)
+
+    post session_url, params: { session: { email: user.email, password: user.password } }
+
+    patch application_url(user)
+
+    assert_redirected_to user_url(user)
+  end
+
+  test "#update updates is_approved to true for the application's user" do
+    user = create(:user, :admin)
+    non_admin = create(:user)
+    
+    post session_url, params: { session: { email: user.email, password: user.password } }
+    
+    cohort = create(:cohort)
+    cohort_params = { cohort: { cohort_id: cohort.id } }
+
+    assert_changes -> { Relationship.count } do
+      patch application_url(non_admin), params: cohort_params
+    end
+
+    assert_redirected_to applications_path
+
+    assert non_admin.reload.is_approved
+  end
+
+  test "#update sets a new relationship for a cohort and assigns a mentor if they are willing_to_mentor" do
+    user = create(:user, :admin)
+    mentor = create(:user, :mentor)
+    
+    post session_url, params: { session: { email: user.email, password: user.password } }
+    
+    cohort = create(:cohort)
+    cohort_params = { cohort: { cohort_id: cohort.id } }
+
+    assert_changes -> { Relationship.count } do
+      patch application_url(mentor), params: cohort_params
+    end
+
+    assert_redirected_to applications_path
+
+    new_relationship = Relationship.last
+
+    assert_equal cohort, new_relationship.cohort
+    assert_equal mentor, new_relationship.mentor
+    assert_nil new_relationship.mentee
+  end
+
+  test "#update sets a new relationship for a cohort and assigns a mentee if they are not willing_to_mentor" do
+    user = create(:user, :admin)
+    mentee = create(:user, :mentee)
+    
+    post session_url, params: { session: { email: user.email, password: user.password } }
+    
+    cohort = create(:cohort)
+    cohort_params = { cohort: { cohort_id: cohort.id } }
+
+    assert_changes -> { Relationship.count } do
+      patch application_url(mentee), params: cohort_params
+    end
+
+    assert_redirected_to applications_path
+
+    new_relationship = Relationship.last
+
+    assert_equal cohort, new_relationship.cohort
+    assert_nil new_relationship.mentor
+    assert_equal mentee, new_relationship.mentee
+  end
 end
