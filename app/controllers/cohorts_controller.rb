f926e91d4b03eeeca5f840c6c21be694bcc6738e
Stella Kim <stella.kim@icloud.com>
2018-03-30 22:28:06 -0400

    CohortsController#show: Render users for each relationship

--- app/controllers/cohorts_controller.rb
+++ app/controllers/cohorts_controller.rb
@@ -14,17 +14,27 @@ class CohortsController < ApplicationController
   end
 
   def show
+    page = params[:page]
+
+    @cohort = Cohort.find(@cohort_id)
+    @relationships = @cohort.relationships.page(page).per(10)
   end
 
   private
 
+  def cohort_id
+    @cohort_id = params[:id]
+  end
+
   def ensure_admin_or_belongs_to_cohort
-    unless current_user.is_admin? || current_user_belongs_to_cohort?(params[:id])
+    cohort_id
+
+    unless current_user.is_admin? || current_user_belongs_to_cohort?
       redirect_to cohorts_url
     end
   end
 
-  def current_user_belongs_to_cohort?(cohort_id)
-    Cohort.by_id_and_user_id(cohort_id, current_user.id).any?
+  def current_user_belongs_to_cohort?
+    Cohort.by_id_and_user_id(@cohort_id, current_user.id).any?
   end
 end
