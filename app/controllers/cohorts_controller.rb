83d717844f325e082c2eef4336281772d5d97fa3
Stella Kim <stella.kim@icloud.com>
2018-03-30 21:58:00 -0400

    Refactor the redirect for CohortsController#show as a before_action

--- app/controllers/cohorts_controller.rb
+++ app/controllers/cohorts_controller.rb
@@ -1,4 +1,6 @@
 class CohortsController < ApplicationController
+  before_action :ensure_admin_or_belongs_to_cohort, only: :show
+
   def index
     page = params[:page]
 
@@ -12,14 +14,17 @@ class CohortsController < ApplicationController
   end
 
   def show
-    unless current_user.is_admin? || belongs_to_cohort?(params[:id])
-      redirect_to cohorts_url
-    end
   end
 
   private
 
-  def belongs_to_cohort?(cohort_id)
+  def ensure_admin_or_belongs_to_cohort
+    unless current_user.is_admin? || current_user_belongs_to_cohort?(params[:id])
+      redirect_to cohorts_url
+    end
+  end
+
+  def current_user_belongs_to_cohort?(cohort_id)
     Cohort.by_id_and_user_id(cohort_id, current_user.id).any?
   end
 end
