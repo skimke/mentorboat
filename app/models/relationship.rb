0deb17b1dbf98c52c8641ba715af54514a8c0f41
Stella Kim <stella.kim@icloud.com>
2018-03-31 17:00:51 -0400

    All fields are optional for Relationship

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,6 +1,6 @@
 class Relationship < ApplicationRecord
-  belongs_to :mentor, class_name: 'User'
-  belongs_to :mentee, class_name: 'User'
+  belongs_to :mentor, class_name: 'User', optional: true
+  belongs_to :mentee, class_name: 'User', optional: true
   belongs_to :cohort, optional: true
   has_many :goals
   has_many :feedbacks
