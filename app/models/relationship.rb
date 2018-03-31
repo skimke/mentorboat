ff610475555cb532e667ac2350d737b2b3810476
Rebecca Qu <beks.qu@gmail.com>
2018-03-31 15:34:49 -0400

    add missing relationships

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -2,4 +2,5 @@ class Relationship < ApplicationRecord
   belongs_to :mentor, class_name: 'User'
   belongs_to :mentee, class_name: 'User'
   belongs_to :cohort, optional: true
+  has_many :goals
 end
