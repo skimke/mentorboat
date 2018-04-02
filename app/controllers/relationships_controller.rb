bae1ce9c4550622f081d458b776a7fcaa005febb
Stella Kim <stella.kim@icloud.com>
2018-04-01 23:30:25 -0400

    Add RelationshipsController #update

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -16,5 +16,26 @@ class RelationshipsController < ApplicationController
   end
 
   def update
+    desired_users_hash = update_params.to_hash.values.reduce(&:merge)
+    relationship = Relationship.find(params[:id])
+
+    Relationship.transaction do
+      relationship.update_attributes!(desired_users_hash)
+
+      Relationship
+        .where.not(id: relationship.id)
+        .where(desired_users_hash.merge(
+          cohort_id: relationship.cohort_id
+        ))
+        .delete_all
+    end
+
+    redirect_to cohort_path(relationship.cohort)
+  end
+
+  private
+
+  def update_params
+    params.permit(mentor: [:mentor_id], mentee: [:mentee_id])
   end
 end
--- test/controllers/relationships_controller_test.rb
+++ test/controllers/relationships_controller_test.rb
@@ -45,4 +45,77 @@ class RelationshipsControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to signed_in_root_url
   end
+
+  test "#update sets user ids for a relationship based on params" do
+    mentee = create(:user, :mentee)
+    cohort = create(:cohort)
+    relationship = create(
+      :relationship,
+      mentee: mentee,
+      cohort: cohort
+    )
+      
+    admin = create(:user, :admin)
+    post session_url, params: { session: { email: admin.email, password: admin.password } }
+
+    mentor = create(:user, :mentor)
+    params = {
+      mentor: { mentor_id: mentor.id },
+      mentee: { mentee_id: mentee.id }
+    }
+    assert_changes -> {
+      Relationship.where(
+        mentee: mentee,
+        mentor: mentor,
+        cohort: cohort
+      ).count
+    }, from: 0, to: 1 do
+      patch relationship_url(relationship), params: params
+    end
+
+    assert_equal mentor, relationship.reload.mentor
+    assert_equal mentee, relationship.mentee
+  end
+
+  test "#update sets user ids for a relationship then removes an already existing relationship with exact same mentor, mentee, cohort" do
+    mentee = create(:user, :mentee)
+    mentor = create(:user, :mentor)
+    cohort = create(:cohort)
+
+    existing_relationship = create(
+      :relationship,
+      mentee: mentee,
+      mentor: mentor,
+      cohort: cohort
+    )
+    relationship = create(
+      :relationship,
+      mentee: mentee,
+      cohort: cohort
+    )
+
+    admin = create(:user, :admin)
+    post session_url, params: { session: { email: admin.email, password: admin.password } }
+
+    params = {
+      mentor: { mentor_id: mentor.id },
+      mentee: { mentee_id: mentee.id }
+    }
+    assert_no_changes -> {
+      Relationship.where(
+        mentee: mentee,
+        mentor: mentor,
+        cohort: cohort
+      ).count
+    } do
+      patch relationship_url(relationship), params: params
+    end
+
+    assert_equal mentor, relationship.reload.mentor
+    assert_equal mentee, relationship.mentee
+    
+    assert_raises ActiveRecord::RecordNotFound do
+      existing_relationship.reload
+    end
+  end
 end
